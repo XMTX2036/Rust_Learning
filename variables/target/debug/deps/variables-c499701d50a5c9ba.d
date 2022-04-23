12f755785998d66f3007d6e9330425273b7732bb
XMTX2036 <754663901@qq.com>
2022-04-23 16:14:38 +0800

    variables learnt

---
+++ variables/target/debug/deps/variables-c499701d50a5c9ba.d
@@ -0,0 +1,5 @@
+/Users/xmtx/Desktop/Code/Rust/Rust_Learning/variables/target/debug/deps/variables-c499701d50a5c9ba: src/main.rs
+
+/Users/xmtx/Desktop/Code/Rust/Rust_Learning/variables/target/debug/deps/variables-c499701d50a5c9ba.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/xmtx/Desktop/Code/Rust/Rust_Learning/variables/target/debug/variables: /Users/xmtx/Desktop/Code/Rust/Rust_Learning/variables/src/main.rs
